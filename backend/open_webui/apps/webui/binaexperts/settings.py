b033e682adb00a9fc9389f31465ff6509b6604ed
Erfan Vahedi <e.vahedi@binaexperts.com>
2024-11-13 18:48:21 +0330

    working on the user feature

--- backend/open_webui/apps/webui/binaexperts/settings.py
+++ backend/open_webui/apps/webui/binaexperts/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SMTP_SERVER = str(os.environ.get("SMTP_SERVER", ""))
-SMTP_PORT = str(os.environ.get("SMTP_PORT", ""))
-SENDER_EMAIL = str(os.environ.get("SENDER_EMAIL", ""))
-SENDER_PASSWORD = str(os.environ.get("SENDER_PASSWORD", ""))
+SMTP_SERVER = str(os.environ.get("SMTP_SERVER", "smtp.elasticemail.com"))
+SMTP_PORT = str(os.environ.get("SMTP_PORT", "2525"))
+SENDER_EMAIL = str(os.environ.get("SENDER_EMAIL", "chat@binaexperts.com"))
+SENDER_PASSWORD = str(os.environ.get("SENDER_PASSWORD", "82F9E2F6AF18208C04F68986D537F9B1FA22"))
